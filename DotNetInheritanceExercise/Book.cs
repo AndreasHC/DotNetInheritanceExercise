416dbee4805644fef7e8ed32eeb9ee5d27e12028
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-03-11 10:31:46 +0100

    Second exercise: book shop

---
+++ DotNetInheritanceExercise/Book.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace DotNetInheritanceExercise
+{
+    public class Book
+    {
+
+        private string author;
+        private string title;
+        private decimal price;
+        protected virtual string Author
+        {
+            get { return author; }
+            set
+            {
+                string[] segments = value.Split(' ');
+                if (segments.Length >= 2)
+                    if (Char.IsDigit(segments[1][0]))
+                        throw new ArgumentException("Author not valid!");
+                author = value;
+            }
+        }
+        protected virtual string Title
+        {
+            get { return title; }
+            set
+            {
+                if (value.Length < 3)
+                    throw new ArgumentException("Title not valid!");
+                title = value;
+            }
+        }
+        protected virtual decimal Price
+        {
+            get { return price; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Price not valid!");
+                price = value;
+            }
+        }
+        public Book(string author, string title, decimal price)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Price = price;
+        }
+        public override string ToString()
+        {
+            var resultBuilder = new StringBuilder();
+            resultBuilder.AppendLine($"Type: {this.GetType().Name}")
+                .AppendLine($"Title: {this.Title}")
+                .AppendLine($"Author: {this.Author}")
+                .AppendLine($"Price: {this.Price:f2}");
+
+            string result = resultBuilder.ToString().TrimEnd();
+            return result;
+        }
+    }
+}
---
+++ DotNetInheritanceExercise/GoldenEditionBook.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNetInheritanceExercise
+{
+    internal class GoldenEditionBook : Book
+    {
+        protected override decimal Price
+        {
+            get
+            {
+                return base.Price * 1.3m;
+            }
+        }
+        public GoldenEditionBook(string author, string title, decimal price) : base(author, title, price)
+        {
+        }
+
+    }
+}
